fbe5be239f0a58972a6a7da084a44ce1446770ef
Xergan <nicolaspqd10@gmail.com>
2024-12-28 19:13:54 +0100

    xs

--- src/components/commands/Experiences.jsx
+++ src/components/commands/Experiences.jsx
@@ -2,14 +2,11 @@ import React from "react";
 
 export default function Experiences() {
   const asciiArt = `
+    Date      Entreprise   Poste       Description      
+------------ ------------ -------- -------------------- 
+ 01/01/1970   Place        Holder   I'm a placeholder.  
 
-  ┌────────────┬────────────┬────────┬────────────────────────┐
-  │    Date    │ Entreprise │ Poste  │      Description       │
-  ├────────────┼────────────┼────────┼────────────────────────┤
-  │ 01/01/1970 │ Place      │ Holder │ This is a placeholder. │
-  └────────────┴────────────┴────────┴────────────────────────┘
-  
-  
+ 
 `;
 
   return (
